4ef3b17f9c90f180156ec465af9ff6e6eda8d64c
Nigel Baillie <nigelbaillie@hey.com>
2024-02-29 13:21:08 +0900

    helix specific stuff

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -6,11 +6,12 @@
 
 #define PREV_TAB &kp LC(PAGE_DOWN)
 #define NEXT_TAB &kp LC(PAGE_UP)
+#define HX_SWAP &kp LA(SEMICOLON)
 
 #define C01 PREV_TAB
 #define C02 NEXT_TAB
 #define C03 &none
-#define C04 &none
+#define C04 HX_SWAP
 #define C05 &none
 #define C06 &none
 
