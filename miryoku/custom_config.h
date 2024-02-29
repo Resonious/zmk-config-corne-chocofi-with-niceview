cfb90195d024687974fa602513fc06be89d9412f
Nigel Baillie <nigelbaillie@hey.com>
2024-02-29 11:19:44 +0900

    try a macro 4

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -4,31 +4,11 @@
 #define MIRYOKU_ALPHAS_QWERTY 1
 #define MIRYOKU_NAV_VI 1
 
-/ {
-    macros {
-        prev_tab: prev_tab {
-            compatible = "zmk,behavior-macro";
-            #binding-cells = <0>;
-            bindings
-                = <&macro_press &kp LCTRL>
-                , <&macro_tap &kp PAGE_DOWN>
-                , <&macro_release &kp LCTRL>
-                ;
-        };
-        next_tab: next_tab {
-            compatible = "zmk,behavior-macro";
-            #binding-cells = <0>;
-            bindings
-                = <&macro_press &kp LCTRL>
-                , <&macro_tap &kp PAGE_UP>
-                , <&macro_release &kp LCTRL>
-                ;
-        };
-    };
-};
+#define PREV_TAB &kp LC(PAGE_DOWN)
+#define NEXT_TAB &kp LC(PAGE_UP)
 
-#define C01 &prev_tab
-#define C02 &next_tab
+#define C01 PREV_TAB
+#define C02 NEXT_TAB
 #define C03 &none
 #define C04 &none
 #define C05 &none
